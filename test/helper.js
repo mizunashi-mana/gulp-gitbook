594b4023830bf591db3397327a3f5b395bee68ca
Mizunashi Mana <mizunashi_mana@mma.club.uec.ac.jp>
2016-10-05 01:07:48 +0900

    Add one test instance

---
+++ test/helper.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var chai = require('chai');
+
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
+
+module.exports = {
+  expect: chai.expect
+};
--- test/isNullable.test.js
+++ test/isNullable.test.js
@@ -1,5 +1,20 @@
 'use strict';
 
+var expect = require('./helper').expect;
+
+var gulpGitbook = require('..');
+var isNullable = gulpGitbook._private.isNullable;
+
 describe('isNullable', function () {
+
+  it('should be ok for nullable values', function () {
+    expect(isNullable(null)).to.be.true;
+    expect(isNullable(undefined)).to.be.true;
+    expect(isNullable(false)).to.be.false;
+    expect(isNullable(0)).to.be.false;
+    expect(isNullable('')).to.be.false;
+    expect(isNullable({})).to.be.false;
+    expect(isNullable(true)).to.be.false;
+  });
 
 });
